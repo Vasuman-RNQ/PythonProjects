494276c6d339cc6524d701c5350a3e88a9f049b6
Vasuman-RNQ <100055990+Vasuman-RNQ@users.noreply.github.com>
2024-06-30 20:45:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask,redirect,url_for
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-@app.route('/home')
-def hello_home():
-    return "<h2> Hello from homepage </h2>"
+from flask import Flask, request, render_template
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/math', methods=['POST'])
+def math_ops():
+    if request.method == 'POST':
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of" + str(num1)+"and"+str(num2) + "is" + str(r)
+        if ops == "subtract":
+            r = num1-num2
+            result = "The subtract of"+str(num1)+"and"+str(num2) + "is"+str(r)
+        if ops == "multiply":
+            r = num1*num2
+            result = "The multiply of"+str(num1)+"and"+str(num2)+"is"+str(r)
+        if ops == "divide":
+            r = num1/num2
+            result = "The divide of "+str(num1)+" and "+str(num2)+" is "+str(r)
+        return render_template("results.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
